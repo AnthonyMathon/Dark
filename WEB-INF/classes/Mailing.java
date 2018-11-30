e851eb51b89d32a4b63a3a261daf33eb587e65b8
AnthonyMathon <mathonanthony@wanadoo.fr>
2018-11-30 16:27:15 +0100

    Update Mailing.java

--- WEB-INF/classes/Mailing.java
+++ WEB-INF/classes/Mailing.java
@@ -7,22 +7,22 @@ import javax.mail.internet.*;
 /**
  *	@author Anthony MATHON
  *  Classe permettant d'envoyer les mails de
- *  notification d'activité sur un compte particulier.
+ *  notification d'activitÃ© sur un compte particulier.
  */
 
 public class Mailing {
 	
 	//Sujets
 	static String confirm = "Confirmation de votre mail";
-	static String warning = "Action étrange sur votre compte";
-	static String blocked = "Votre compte a été bloqué";
+	static String warning = "Action Ã©trange sur votre compte";
+	static String blocked = "Votre compte a Ã©tÃ© bloquÃ©";
 	
 	private static int port = 465;
 	private static String host = "smtp.orange.fr";
-	private static String from = "mathonanthony@wanadoo.fr";
+	private static String from = "";
 	private static boolean auth = true;
-	private static String username = "mathonanthony@wanadoo.fr";
-	private static String password = "Passw0rd";
+	private static String username = "";
+	private static String password = "";
 	private static Protocol protocol = Protocol.SMTPS;
 	private static boolean debug = true;
 	
@@ -75,10 +75,10 @@ public class Mailing {
 	}
 	
 	public static void sendConfirmMail(String mail, String name) throws IOException {
-		System.out.println("(mail)tentative d'envoi de mail de confirmation à "+name+" via l'adresse "+mail);
+		System.out.println("(mail)tentative d'envoi de mail de confirmation Ã  "+name+" via l'adresse "+mail);
 		String body = "Bonjour. \n"
-				+ "Vous avez récemment demandé a valider votre e-mail afin d'être tenu informé des actions potentiellement illégitimes sur votre compte.\n"
-				+ "Veuillez dès maintenant valider votre mail en cliquant sur le lien ci dessous :\n"
+				+ "Vous avez rÃ©cemment demandÃ© a valider votre e-mail afin d'Ãªtre tenu informÃ© des actions potentiellement illÃ©gitimes sur votre compte.\n"
+				+ "Veuillez dÃ¨s maintenant valider votre mail en cliquant sur le lien ci dessous :\n"
 				+ "http://serveurdarkpit59.servegame.com:9876/Dark/Confirm?mail="+mail+"\n"
 				+ "\n"
 				+ "Cordialement, \n"
@@ -87,10 +87,10 @@ public class Mailing {
 	}
 
 	public static void sendWarningMail(String mail, String name) throws IOException {
-		System.out.println("(mail)tentative d'envoi de mail d'avertissement à "+name+" via l'adresse "+mail);
+		System.out.println("(mail)tentative d'envoi de mail d'avertissement Ã  "+name+" via l'adresse "+mail);
 		String body = "Bonjour. \n"
-				+ "Une adresse IP inhabituelle s'est récemment connectée à votre compte.\n"
-				+ "Afin de vérifier que c'était vous, veuillez cliquer sur ce lien :\n"
+				+ "Une adresse IP inhabituelle s'est rÃ©cemment connectÃ©e Ã  votre compte.\n"
+				+ "Afin de vÃ©rifier que c'Ã©tait vous, veuillez cliquer sur ce lien :\n"
 				+ "http://serveurdarkpit59.servegame.com:9876/Dark/Justify?mail="+mail+"\n"
 				+ "\n"
 				+ "Cordialement, \n"
@@ -99,13 +99,13 @@ public class Mailing {
 	}
 	
 	public static void sendBlockingMail(String mail, String name) throws IOException {
-		System.out.println("(mail)tentative d'envoi de mail de blocage à "+name+" via l'adresse "+mail);
+		System.out.println("(mail)tentative d'envoi de mail de blocage Ã  "+name+" via l'adresse "+mail);
 		String body = "Bonjour. \n"
-				+ "Une personne potentiellement malintentionnée a récemment tenté de se connecter à votre compte.\n"
-				+ "Votre compte a, en effet, tenté de se connecter avec un mot de passe incorrect à plusieurs reprises.\n"
-				+ "Afin de déverrouiller votre compte et pouvoir vous connecter à nouveau, veuillez cliquer sur ce lien :\n"
+				+ "Une personne potentiellement malintentionnÃ©e a rÃ©cemment tentÃ© de se connecter Ã  votre compte.\n"
+				+ "Votre compte a, en effet, tentÃ© de se connecter avec un mot de passe incorrect Ã  plusieurs reprises.\n"
+				+ "Afin de dÃ©verrouiller votre compte et pouvoir vous connecter Ã  nouveau, veuillez cliquer sur ce lien :\n"
 				+ "http://serveurdarkpit59.servegame.com:9876/Dark/Unblock?mail="+mail+"\n"
-				+ "Pensez par ailleurs à modifier votre Mot de passe.\n"
+				+ "Pensez par ailleurs Ã  modifier votre Mot de passe.\n"
 				+ "\n"
 				+ "Cordialement, \n"
 				+ "Anthony MATHON, Administrateur";
@@ -115,8 +115,8 @@ public class Mailing {
 	//mails
 	
 			/*"Bonjour. \n"
-			+ "Vous avez récemment demandé a valider votre e-mail afin d'être tenu informé des actions potentiellement illégitimes sur votre compte.\n"
-			+ "Veuillez dès maintenant valider votre mail en cliquant sur le lien ci dessous :\n"
+			+ "Vous avez rÃ©cemment demandÃ© a valider votre e-mail afin d'Ãªtre tenu informÃ© des actions potentiellement illÃ©gitimes sur votre compte.\n"
+			+ "Veuillez dÃ¨s maintenant valider votre mail en cliquant sur le lien ci dessous :\n"
 			+ "http://serveurdarkpit59.servegame.com:9876/Dark/Confirm?mail="+mail+"\n"
 			+ "\n"
 			+ "Cordialement, \n"
@@ -124,8 +124,8 @@ public class Mailing {
 	
 	
 			/*"Bonjour. \n"
-			+ "Une adresse IP inhabituelle s'est récemment connectée à votre compte.\n"
-			+ "Afin de vérifier que c'était vous, veuillez cliquer sur ce lien :\n"
+			+ "Une adresse IP inhabituelle s'est rÃ©cemment connectÃ©e Ã  votre compte.\n"
+			+ "Afin de vÃ©rifier que c'Ã©tait vous, veuillez cliquer sur ce lien :\n"
 			+ "http://serveurdarkpit59.servegame.com:9876/Dark/Justify?mail="+mail+"\n"
 			+ "\n"
 			+ "Cordialement, \n"
@@ -133,11 +133,11 @@ public class Mailing {
 	
 	
 			/*"Bonjour. \n"
-			+ "Une personne potentiellement malintentionnée a récemment tenté de se connecter à votre compte.\n"
-			+ "Votre compte a, en effet, tenté de se connecter avec un mot de passe incorrect à plusieurs reprises.\n"
-			+ "Afin de déverrouiller votre compte et pouvoir vous connecter à nouveau, veuillez cliquer sur ce lien :\n"
+			+ "Une personne potentiellement malintentionnÃ©e a rÃ©cemment tentÃ© de se connecter Ã  votre compte.\n"
+			+ "Votre compte a, en effet, tentÃ© de se connecter avec un mot de passe incorrect Ã  plusieurs reprises.\n"
+			+ "Afin de dÃ©verrouiller votre compte et pouvoir vous connecter Ã  nouveau, veuillez cliquer sur ce lien :\n"
 			+ "http://serveurdarkpit59.servegame.com:9876/Dark/Unblock?mail="+mail+"\n"
-			+ "Pensez par ailleurs à modifier votre Mot de passe.\n"
+			+ "Pensez par ailleurs Ã  modifier votre Mot de passe.\n"
 			+ "\n"
 			+ "Cordialement, \n"
 			+ "Anthony MATHON, Administrateur"*/
